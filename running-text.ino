68c1a31c7129a4aa886ffa905347b7905e72f6b6
lutpiero <lutpiero@gmail.com>
2026-05-25 14:10:33 +0700

    Refactor MD_Parola initialization and pin setup
    Updated pin definitions and initialization method for MD_Parola display.

--- running-text.ino
+++ running-text.ino
@@ -6,36 +6,27 @@
 // Jika teks terbalik atau berantakan, coba ganti ke GENERIC_HW atau PAROLA_HW.
 #define HARDWARE_TYPE MD_MAX72XX::FC16_HW
 
-// Jumlah blok matrix 8x8 yang berjejer (biasanya 4 kalau pakai modul standar)
 #define MAX_DEVICES 4 
 
-// Definisikan pin CS sesuai yang kita obrolkan tadi (D8 pada NodeMCU)
-#define CS_PIN D8 
+// Gunakan angka pin yang persis sama dengan kode lama Anda
+#define DATA_PIN 13
+#define CLK_PIN  14
+#define CS_PIN   15
 
-// Inisialisasi object Parola menggunakan Hardware SPI
-// (DIN otomatis ke D7, CLK otomatis ke D5)
-MD_Parola myDisplay = MD_Parola(HARDWARE_TYPE, CS_PIN, MAX_DEVICES);
+// Inisialisasi menggunakan mode Software SPI (menyebutkan DATA, CLK, dan CS secara spesifik)
+MD_Parola myDisplay = MD_Parola(HARDWARE_TYPE, DATA_PIN, CLK_PIN, CS_PIN, MAX_DEVICES);
 
 void setup() {
-  // Memulai display
   myDisplay.begin();
-  
-  // Mengatur tingkat kecerahan LED (0 sampai 15)
-  myDisplay.setIntensity(5);
-  
-  // Membersihkan layar sebelum mulai
-  myDisplay.displayClear();
+  myDisplay.setIntensity(5); // Tingkat kecerahan
+  myDisplay.displayClear();  // Membersihkan layar
 
-  // Mengatur parameter teks berjalan:
-  // (Teks, Perataan, Kecepatan, Jeda (ms), Efek Masuk, Efek Keluar)
-  // Kecepatan: Semakin kecil angkanya, semakin cepat jalannya (misal 50).
+  // Mengatur teks berjalan
   myDisplay.displayText("Selamat Iedul Adha 1447 H", PA_CENTER, 75, 0, PA_SCROLL_LEFT, PA_SCROLL_LEFT);
 }
 
 void loop() {
-  // Menjalankan animasi secara terus menerus
   if (myDisplay.displayAnimate()) {
-    // Kalau animasi selesai, reset agar mengulang dari awal
     myDisplay.displayReset();
   }
 }
